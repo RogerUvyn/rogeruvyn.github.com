2bf6948b841df67127308e4ed16255f80251c938
Roger Uvyn <roger.uvyn@labor.ny.gov>
2012-12-03 11:56:56 -0500

    First bootstrap edition
    First bootstrap edition

---
+++ personal.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Roger Uvyn's Personal Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+    <!-- Including the top navigation menue -->
+    <?php include("assets/includes/topNav.php"); ?>
+
+    <div class="hero-unit">
+
+    <p>
+      <a class="btn btn-primary btn-large" href="personal/resume/RogerUvynRes.pdf">My Resume &raquo;</a>
+      <a class="btn btn-primary btn-large" href="http://www.linkedin.com/profile/edit?goback=%2Enmp_*1_*1_*1_*1_*1_*1_*1_*1_*1&trk=spm_pic">My Linkedin &raquo;</a>
+      <a class="btn btn-primary btn-large" href="http://www.facebook.com/roger.uvyn.9?ref=tn_tnmn">Face Book &raquo;</a>
+      <a class="btn btn-primary btn-large" href="personal/interests.php">Interests &raquo;</a>
+    </p>
+
+    </div>
+
+
+    <div class="container">
+
+
+
+
+      <hr>
+
+      <!-- Including the footer -->
+      <?php include("assets/includes/footer.php"); ?>
+
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
---
+++ personal/interests.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Roger Uvyn's Hobbies and Interests</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Fav and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+  </head>
+
+  <body>
+
+    <!-- Including the top navigation menue -->
+    <?php include("../assets/includes/topNav.php"); ?>
+
+    <div class="hero-unit">
+
+    <ul class="nav">
+
+
+
+
+
+             <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Hobbies <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li class="nav-header">Automotive</li>
+                    <li class="dropdown-submenu">
+                    <a tabindex="-1" href="#">Automotive Racing</a>
+                        <ul class="dropdown-menu">
+                        <li><a href="#">NHRA</a></li>
+                        <li><a href="#">NASCAR</a></li>
+                        </ul>
+                    </li>
+                  <li><a href="#">Automotive Restoration</a></li>
+                  <li><a href="#">Muscle Cars</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Other</li>
+                  <li><a href="#">RC Planes</a></li>
+                  <li><a href="#">RC Cars</a></li>
+                  <li><a href="#">Painting</a></li>
+                </ul>
+              </li>
+            </ul>
+
+
+
+    </div>
+
+
+    <div class="container">
+
+
+
+
+      <hr>
+
+      <!-- Including the footer -->
+      <?php include("../assets/includes/footer.php"); ?>
+
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
